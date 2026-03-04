9299d78b3f5ef4df79207c836384df2ac283cf17
Aliaksei Papou <aliaksei.papou1@gmail.com>
2026-03-04 14:22:05 +0200

    small fix

--- Final-Project/SA_ONLINE/src_apple_online.sql
+++ Final-Project/SA_ONLINE/src_apple_online.sql
@@ -7,14 +7,27 @@ INSERT INTO SA_APPLE_ONLINE.SRC_APPLE_ONLINE (
     order_code, order_time, year, quarter, month, sku_id, product_name, category_id, category_name, sub_category_name,
     model_year, color, storage_capacity, warranty_period, customer_id, first_name, last_name, age, gender, segment,
     email, phone, address_id, address, country, region, city, district, postal_code, promotion_id,
-    promotion_name, promotion_type, is_active, promo_start_date, promo_end_date, discount_value, payment_id,
-    payment_method_name, payment_type, payment_date, payment_status, list_price, unit_cost, qty, insert_dt
+    promotion_name, promotion_type, is_active, promo_start_date, promo_end_date, discount_value, 
+    payment_method_name, payment_type, list_price, unit_cost, qty, insert_dt
 )
 SELECT 
     order_code, order_time, year, quarter, month, sku_id, product_name, category_id, category_name, sub_category_name,
     model_year, color, storage_capacity, warranty_period, customer_id, first_name, last_name, age, gender, segment,
     email, phone, address_id, address, country, region, city, district, postal_code, promotion_id,
-    promotion_name, promotion_type, is_active, promo_start_date, promo_end_date, discount_value, payment_id,
-    payment_method_name, payment_type, payment_date, payment_status, list_price, unit_cost, qty, CURRENT_TIMESTAMP
+    promotion_name, promotion_type, is_active, promo_start_date, promo_end_date, discount_value, 
+    payment_method_name, payment_type, list_price, unit_cost, qty, CURRENT_TIMESTAMP
 FROM SA_APPLE_ONLINE.EXT_APPLE_ONLINE;
 COMMIT;
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Final-Project/SA_POS/src_apple_pos.sql
+++ Final-Project/SA_POS/src_apple_pos.sql
@@ -10,15 +10,17 @@ INSERT INTO SA_APPLE_POS.SRC_APPLE_POS (
     model_year, color, storage_capacity, warranty_period, store_id, store_name, store_type, store_status, store_size_sqm, opening_year,
     has_genius_bar, manager_id, employee_id, employee_first_name, employee_last_name, position, hire_date, employee_email, employee_phone,
     loyalty_card_no, address_id, address, global_region, country, state, city, district, postal_code, promotion_id, promotion_name,
-    promotion_type, is_active, promo_start_date, promo_end_date, discount_value, payment_id, payment_method_name, payment_type,
-    payment_date, payment_status, unit_price, unit_cost, quantity, insert_dt
+    promotion_type, is_active, promo_start_date, promo_end_date, discount_value, payment_method_name, payment_type,
+    unit_price, unit_cost, quantity, insert_dt
 )
 SELECT 
     sale_id, sale_date, year, quarter, month, product_id, product_name, category_id, category_name, sub_category_name,
     model_year, color, storage_capacity, warranty_period, store_id, store_name, store_type, store_status, store_size_sqm, opening_year,
     has_genius_bar, manager_id, employee_id, employee_first_name, employee_last_name, position, hire_date, employee_email, employee_phone,
     loyalty_card_no, address_id, address, global_region, country, state, city, district, postal_code, promotion_id, promotion_name,
-    promotion_type, is_active, promo_start_date, promo_end_date, discount_value, payment_id, payment_method_name, payment_type,
-    payment_date, payment_status, unit_price, unit_cost, quantity, CURRENT_TIMESTAMP
+    promotion_type, is_active, promo_start_date, promo_end_date, discount_value, payment_method_name, payment_type,
+    unit_price, unit_cost, quantity, CURRENT_TIMESTAMP
 FROM SA_APPLE_POS.EXT_APPLE_POS;
 COMMIT;
+
+
